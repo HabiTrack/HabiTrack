e01f99fb393bdbb6bd02c3032a595cb88d93f761
Jake Bisson <jbiss085@uottawa.ca>
2021-01-30 13:34:44 -0500

    Updated database schema

---
+++ server/models/routineModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const routineSchema = new mongoose.Schema({
+    date: {type: Date, default: Date.now},
+    habits: {type: Array, default: []},
+    datecreated: {type: Date, default: Date.now},
+    dateupdated: {type: Date, default: Date.now}
+});
+
+module.exports = routineSchema = mongoose.model("habit", routineSchema);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     lastname: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    routines: {type: Array, default: []},
     datecreated: {type: Date, required: true, default: Date.now},
     dateupdated: {type: Date, required: true, default: Date.now},
     isDeleted: {type: Boolean, default: false}

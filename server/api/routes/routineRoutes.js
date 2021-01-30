a85436b066df365c992456a47fea8a5a3982b103
Jake Bisson <jbiss085@uottawa.ca>
2021-01-30 14:09:53 -0500

    Modified db schema and modified route files

--- server/api/routes/routineRoutes.js
+++ server/api/routes/routineRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const validation = require('../../validation/validation');
+const tokenauth = require('../../middleware/tokenauth');
 
-const Habit = require('../../models/habitModel');
+const Routine = require('../../models/habitModel');
 
 //test route for api habit routes
 router.get("/test", (req, res) => {
     res.status(200).send("Welcome to the api: Habit routes")
+});
+
+//route for creating a new habit
+router.post("/create", tokenauth.verifyToken, (req, res)=>{
+    //user id: 
 });
 
 module.exports = router;

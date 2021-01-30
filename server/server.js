5036e405eb2086bcc63a2721d4c7abb80d9d78b6
Jake Bisson <jbiss085@uottawa.ca>
2021-01-29 23:12:56 -0500

    Added server.js, setup folder structure

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+//set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//connect to mongo db via mongoose
+mongoose.connect(process.env.MONGODB_CONNECT, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true, 
+}, (err) => {
+    if (err) {
+        console.log("Mongo DB failed to connect!");
+        throw err;
+    } else {
+        console.log("Mongo DB connected!");
+    }
+});
+
+//api routes
+//app.use("/api/users", require("./routes/api/userRouter"));
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Express server listening on port: ${PORT}`);
+});

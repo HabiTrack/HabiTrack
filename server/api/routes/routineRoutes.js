94faa57b78a59d3c3275be1097baab43f0fde7d4
Braden Collingwood <bradencollingwood82@gmail.com>
2021-01-30 18:34:57 -0500

    getRoutines

--- server/api/routes/routineRoutes.js
+++ server/api/routes/routineRoutes.js
@@ -1,75 +1,84 @@
-const router = require('express').Router();
-const validation = require('../../validation/validation');
-const tokenauth = require('../../middleware/tokenauth');
-const Routine = require('../../models/routineModel');
-const User = require('../../models/userModel');
+const router = require("express").Router();
+const validation = require("../../validation/validation");
+const tokenauth = require("../../middleware/tokenauth");
+const Routine = require("../../models/routineModel");
+const User = require("../../models/userModel");
+const querystring = require("querystring");
 
 //test route for api habit routes
 router.get("/test", (req, res) => {
-    res.status(200).send("Welcome to the api: routine routes")
+  res.status(200).send("Welcome to the api: routine routes");
 });
 
 //route for creating a new habit
-router.post("/addhabit", tokenauth.verifyToken, async (req, res)=>{
-    try {
-        //validate fields
-        const {errors, isValid} = await validation.validateCreateHabit(req.body);
-        //if fields are not valid, send error back with error messages
-        if (!isValid) {
-            return res.status(400).json({errors: errors, isValid: isValid});
-        }
-
-        //build habit template
-        let habit = {};
-        habit.title = req.body.title;
-        habit.trigger = req.body.trigger;
-        habit.type = req.body.type;
-        habit.duration = req.body.duration;
-        habit.completed = false;
-        habit.durationCompleted = null;
-
-        //retrieve the list of user routines
-        let user = await User.findOne({_id: req.body.id});
-        let routines = user.routines;
-        let currentRoutineID = routines[routines.length-1];
-
-        //push the new habit onto the routine's habit array
-        await Routine.updateOne({_id: currentRoutineID}, {$push: {habits: habit}});
-
-        return res.status(200).json({habit});
-    } catch (err) {
-        res.status(500).json({serverError: err.message, isValid: false});
+router.post("/addhabit", tokenauth.verifyToken, async (req, res) => {
+  try {
+    //validate fields
+    const { errors, isValid } = await validation.validateCreateHabit(req.body);
+    //if fields are not valid, send error back with error messages
+    if (!isValid) {
+      return res.status(400).json({ errors: errors, isValid: isValid });
     }
+
+    //build habit template
+    let habit = {};
+    habit.title = req.body.title;
+    habit.trigger = req.body.trigger;
+    habit.type = req.body.type;
+    habit.duration = req.body.duration;
+    habit.completed = false;
+    habit.durationCompleted = null;
+
+    //retrieve the list of user routines
+    let user = await User.findOne({ _id: req.body.id });
+    let routines = user.routines;
+    let currentRoutineID = routines[routines.length - 1];
+
+    //push the new habit onto the routine's habit array
+    await Routine.updateOne(
+      { _id: currentRoutineID },
+      { $push: { habits: habit } }
+    );
+
+    return res.status(200).json({ habit });
+  } catch (err) {
+    res.status(500).json({ serverError: err.message, isValid: false });
+  }
 });
 
 // Get a user's habits
 router.get("/gethabits", tokenauth.verifyToken, async (req, res) => {
-    try {
-        // Check if an id was provided
-        if (!req.body.id) {
-            console.log("User id was not provided.");
-            return res.sendStatus(500);
-        }
+  try {
+    // Check if an id was provided
 
-        // Fetch the user's routine ids
-        let userData = await User.findOne({_id: req.body.id}, {routines: 1});
-        let userRoutines = userData.routines;
-
-        // Create an array of rountine ids
-        let conditions = [];
-        for (let i = 0; i < userRoutines.length; i++) {
-            conditions.push({_id: userRoutines[i]});
-        }
-
-        // Fetch routines
-        let routineObjs = await Routine.find({$or: conditions}, {date: 1, habits: 1});
-
-        // Return rountines
-        return res.status(200).json({routines: routineObjs});
-    } catch (err) {
-        console.error(err);
-        return res.sendStatus(500); 
+    console.log(req.query);
+    if (!req.query.id) {
+      console.log("User id was not provided.");
+      return res.sendStatus(500);
     }
+
+    // Fetch the user's routine ids
+    let userData = await User.findOne({ _id: req.query.id }, { routines: 1 });
+    let userRoutines = userData.routines;
+
+    // Create an array of rountine ids
+    let conditions = [];
+    for (let i = 0; i < userRoutines.length; i++) {
+      conditions.push({ _id: userRoutines[i] });
+    }
+
+    // Fetch routines
+    let routineObjs = await Routine.find(
+      { $or: conditions },
+      { date: 1, habits: 1 }
+    );
+
+    // Return rountines
+    return res.status(200).json({ routines: routineObjs });
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = router;
